ff067b42583bed8977dfb04e7ff069a467826331
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-06-01 12:40:36 -0700

    First commit to reflect added files and project configuration.

---
+++ Swiftris/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Swiftris
+//
+//  Created by Joao Paulo Galvao Alves on 6/1/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ Swiftris/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  Swiftris
+//
+//  Created by Joao Paulo Galvao Alves on 6/1/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
