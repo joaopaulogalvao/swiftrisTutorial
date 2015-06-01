66d31e3d6a3df5f805b71f3f7fd552beb4a12699
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-06-01 15:41:12 -0700

    Create rows and columns in Array2D.swift which will be stored in the array.

--- Swiftris/Array2D.swift
+++ Swiftris/Array2D.swift
@@ -6,4 +6,35 @@
 //  Copyright (c) 2015 Bloc. All rights reserved.
 //
 
-import Foundation
+    //#1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    //#2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int){
+        self.columns = columns
+        self.rows = rows
+        
+    //#3
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+        
+    }
+    
+    //#4
+    subscript(column:Int, row: Int) -> T? {
+        
+        get {
+            return array[(row * column) + column]
+        }
+        
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+        
+    }
+    
+    
+}
